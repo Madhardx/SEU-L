633d9ef5bced5649289d9b97671aa507e5db21b3
Madhardx <dawid.rybak@onet.eu>
2024-01-23 11:37:47 +0100

    Wstep

---
+++ templates_c/03f034b2316c9f0b3c259fe03490ce520eaf7064_0.file.foot.tpl.php
@@ -0,0 +1,32 @@
+<?php
+/* Smarty version 4.3.4, created on 2024-01-23 11:34:16
+  from 'G:\xampp\htdocs\SEU-L\templates\foot.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.4',
+  'unifunc' => 'content_65af9628b61be3_19233680',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '03f034b2316c9f0b3c259fe03490ce520eaf7064' => 
+    array (
+      0 => 'G:\\xampp\\htdocs\\SEU-L\\templates\\foot.tpl',
+      1 => 1706005942,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_65af9628b61be3_19233680 (Smarty_Internal_Template $_smarty_tpl) {
+?>    
+<?php echo '<script'; ?>
+ src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+</body>
+
+</html><?php }
+}
---
+++ templates_c/7d0e20d8d2125e335265673e469a8c67c584b496_0.file.index.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/* Smarty version 4.3.4, created on 2024-01-23 11:35:30
+  from 'G:\xampp\htdocs\SEU-L\templates\index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.4',
+  'unifunc' => 'content_65af96728363a3_52756260',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '7d0e20d8d2125e335265673e469a8c67c584b496' => 
+    array (
+      0 => 'G:\\xampp\\htdocs\\SEU-L\\templates\\index.tpl',
+      1 => 1706006126,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:head.tpl' => 1,
+    'file:foot.tpl' => 1,
+  ),
+),false)) {
+function content_65af96728363a3_52756260 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_subTemplateRender("file:head.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+$_smarty_tpl->_subTemplateRender("file:foot.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+}
+}
