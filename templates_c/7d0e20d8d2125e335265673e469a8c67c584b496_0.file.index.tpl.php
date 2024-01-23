5f375b1de6e61c01f7538101f5734a37340c7d99
Madhardx <dawid.rybak@onet.eu>
2024-01-23 12:26:05 +0100

    Logowanie

--- templates_c/7d0e20d8d2125e335265673e469a8c67c584b496_0.file.index.tpl.php
+++ templates_c/7d0e20d8d2125e335265673e469a8c67c584b496_0.file.index.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.4, created on 2024-01-23 11:35:30
+/* Smarty version 4.3.4, created on 2024-01-23 12:23:59
   from 'G:\xampp\htdocs\SEU-L\templates\index.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.4',
-  'unifunc' => 'content_65af96728363a3_52756260',
+  'unifunc' => 'content_65afa1cfad3516_77568921',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '7d0e20d8d2125e335265673e469a8c67c584b496' => 
     array (
       0 => 'G:\\xampp\\htdocs\\SEU-L\\templates\\index.tpl',
-      1 => 1706006126,
+      1 => 1706009035,
       2 => 'file',
     ),
   ),
@@ -22,8 +22,34 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:foot.tpl' => 1,
   ),
 ),false)) {
-function content_65af96728363a3_52756260 (Smarty_Internal_Template $_smarty_tpl) {
+function content_65afa1cfad3516_77568921 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:head.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
-$_smarty_tpl->_subTemplateRender("file:foot.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+<div class="container">
+    <div class="row">
+        <div class="col col-lg-4 offset-lg-4 mt-3 mb-2">
+            <div class="text-center">
+                <img src="logo.png" class="img-fluid">
+            </div>
+            <div class="mb-3">
+                <h2 class="text-center">Zaloguj się</h2>
+            </div>
+            <form action="index.php">
+                <div class="mb-5 mt-5">
+                    <label for="Login" class="form-label">Login</label>
+                    <input type="text" class="form-control" id="Login" placeholder="Wpisz swój login">
+                </div>
+                <div class="mb-5 mt-5"">
+                <label for=" Password" class="form-label">Hasło</label>
+                    <input type="text" class="form-control" id="password" placeholder="">
+                </div>
+                <div>
+                    <button type="submit" class="btn btn-primary w-100">Zaloguj się</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<?php $_smarty_tpl->_subTemplateRender("file:foot.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 }
 }
